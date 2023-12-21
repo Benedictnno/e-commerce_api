49a842715d5d4d20d71e9b59f3081d0b3ea03776
Benedict <Benedictnnaoma0@gmail.com>
2023-12-21 23:29:16 +0100

    logout route

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,17 +13,36 @@ const register = async (req, res) => {
   const role = isFirstAccount ? "admin" : "user";
   const user = await UserModel.create({ email, password, name, role });
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
-  attachCookiesToResponse({res,tokenUser})
+  attachCookiesToResponse({ res, tokenUser });
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
-
 };
 
 const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+  const user = UserModel.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("invalid credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("incorrect password");
+  }
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
-  res.send("logOut");
+  res.cookie('token',"logOut",{
+     httpOnly: true,
+    expires: new Date(Date.now()),
+    secure:process.env.NODE_ENV === "production",
+    signed:true,
+  });
 };
 
 module.exports = { register, login, logout };
