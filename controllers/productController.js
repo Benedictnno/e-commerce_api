107aa0f29f1645c5b826580f165ccb06ec37f3a6
Benedict <Benedictnnaoma0@gmail.com>
2023-12-30 15:15:54 +0100

    product route cond.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,36 +1,54 @@
 const { StatusCodes } = require("http-status-codes");
 const Product = require("../models/Product");
+const CustomAPIError = require("../errors");
 
-const createProduct = async(req, res) => {
-  const {
-    name,
-    price,
-    description,
-    image,
-    category,
-    company,
-    colors,
-    featured,
-    freeShipping,
-    inventory,
-  } = req.body;
-  const product = await Product.create(req.body)
-  res.status(StatusCodes.CREATED).json({product});
+const createProduct = async (req, res) => {
+  req.body.user = req.user.userId;
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ product });
+};
 
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products, count: products.length });
 };
-const getAllProducts = async(req, res) => {
-  res.send("getAllProducts");
+
+const getSingleProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findOne({ _id: id });
+  if (!product)
+    throw new CustomAPIError.NotFoundError(
+      `Product with id of ${id} does not exist`
+    );
+
+  res.status(StatusCodes.OK).json({ product });
 };
-const getSingleProduct = async(req, res) => {
-  res.send("getSingleProduct");
+
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!product)
+    throw new CustomAPIError.NotFoundError(
+      `Product with id of ${id} does not exist`
+    );
+  res.status(StatusCodes.OK).json({ product });
 };
-const updateProduct = async(req, res) => {
-  res.send("updateProduct");
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findOne({ _id: id });
+  if (!product)
+    throw new CustomAPIError.NotFoundError(
+      `Product with id of ${id} does not exist`
+    );
+    await product.remove()
+  res.status(StatusCodes.GONE).json({ msg:'Success! Product remove' });
 };
-const deleteProduct = async(req, res) => {
-  res.send("deleteProduct");
-};
-const uploadImage = async(req, res) => {
+
+const uploadImage = async (req, res) => {
   res.send("uploadImage");
 };
 
@@ -40,5 +58,5 @@ module.exports = {
   getSingleProduct,
   updateProduct,
   uploadImage,
-  deleteProduct
+  deleteProduct,
 };
--- models/Product.js
+++ models/Product.js
@@ -39,6 +39,7 @@ const ProductSchema = new mongoose.Schema(
     },
     colors: {
       type: [String],
+      default:['#222'],
       required: true,
     },
     featured: {
