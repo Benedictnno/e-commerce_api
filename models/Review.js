1aace14789d08d6957a32b9c6d98888dd373c068
Benedict <Benedictnnaoma0@gmail.com>
2024-01-02 18:23:15 +0100

    reviews schema

---
+++ models/Review.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "please provide rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "please provide a comment title"],
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+
+      required: [true, "please provide a valid comment"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "products",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model("reviews", ReviewSchema);
